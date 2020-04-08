e8221509f12ee7368f0a735120e73aaef4d15f13
Riccardo Ravasio <ravasio@Riccardos-MacBook-Pro.local>
2020-04-08 15:47:27 +0200

    Updated independent model

--- mutation_costs/independentsite_model.m
+++ mutation_costs/independentsite_model.m
@@ -1,21 +1,37 @@
 %% Independent-site model
 
 h=zeros(402,1);
-DE_tmp=zeros(402,1);
+DE_av=zeros(402,1);
 DE_calculated=zeros(402,1);
+
 Mend=130000;
 sb = 360/408;
 
 M = size(X,1);
 f = (1/M)*(sum(X));
 
-%hi = real(atanh(2*f-1));
+%% Field of the standard indepedent-site model
+hi = real(atanh(2*f-1));
 
-for m = 1:Mend
+for i = 1:402
+    DE_calculated(i) = (2*f(i)-1)*hi(i);       % estimation conservation
+end
+
+%% Field with background frequency
 for i = 1:402
     h(i)=log(f(i)*(1-sb)/(sb*(1-f(i)))); %field
-    DE_tmp(i) = (2*X(m,i)-1)*h(i);       %mutation cost
 end
-DE_calculated = DE_calculated + DE_tmp;
+
+for i=1:402
+ DE_av(i) = (2*f(i)*(1-sb)/((1-f(i))*sb + f(i)*(1-sb)) - 1)*h(i);
 end
-DE_calculated = DE_calculated/Mend;
+
+%% Conservation of Halabi et al.
+cons_Halabi=zeros(402,1);
+for i=1:402
+ cons_Halabi(i) = f(i)*log(f(i)/sb) + (1-f(i))*log((1-f(i))/(1-sb));
+end
+
+
+
+
